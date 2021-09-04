cd47e07903951220a84134abb39cb4682891a2e7
Trevor <tbuhler347@gmail.com>
2021-09-03 18:03:32 -0600

    Initial Commit

---
+++ The Real Ish/The_Real_IshApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Real_IshApp.swift
+//  The Real Ish
+//
+//  Created by Wendy Buhler on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Real_IshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
